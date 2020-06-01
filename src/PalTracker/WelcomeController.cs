07741fcc6f8373c78f05e8e1ca484def770918e0
Suneetha <inturi.suneetha@gmail.com>
2020-06-01 07:38:48 +0000

    Asp.net core web api 1.0

---
+++ src/PalTracker/WelcomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello() => "hello";
+    }
+}
