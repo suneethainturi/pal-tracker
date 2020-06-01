03401cf37425c706f6e6d975cee1fdb606da17e3
Suneetha <inturi.suneetha@gmail.com>
2020-06-01 10:47:23 +0000

    External config 1.0

--- test/PalTrackerTests/IntegrationTestServer.cs
+++ test/PalTrackerTests/IntegrationTestServer.cs
@@ -6,7 +6,13 @@ namespace PalTrackerTests
 {
     public static class IntegrationTestServer
     {
-        public static TestServer Start() =>
-            new WebApplicationFactory<Program>().Server;
+              public static TestServer Start()
+       {            if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("WELCOME_MESSAGE")))
+            {
+                System.Environment.SetEnvironmentVariable("WELCOME_MESSAGE", "Default message");
+           }
+
+           return new WebApplicationFactory<Program>().Server;
+       }
     }
 }
